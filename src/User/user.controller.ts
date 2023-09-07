0c93e40af2b7e74981a3db2b4272bfad23f9565c
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-07 13:46:16 -0300

    refactor: module users

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -11,44 +11,46 @@ import {
 
 @Controller('users')
 export class UserController {
-  @Post()
-  async create(@Body() body) {
-    return { body };
-  }
   @Get()
   async list() {
     return {
       users: [],
     };
   }
+
   @Get(':id')
   async show(@Param() params) {
     return {
-      ...params,
+      params,
       user: {},
     };
   }
-  @Put(':id')
-  async updateAll(@Param() params, @Body() body) {
-    return {
-      method: 'put',
-      params,
-      body,
-    };
+
+  @Post()
+  async create(@Body() body) {
+    return { body };
   }
-  @Patch(':id')
+
+  @Put(':id')
   async update(@Param() params, @Body() body) {
     return {
-      method: 'patch',
+      method: 'PUT',
       params,
       body,
     };
   }
+
+  @Patch(':id')
+  async updatePartial(@Param() params, @Body() body) {
+    return {
+      method: 'PATCH',
+      params,
+      body,
+    };
+  }
+
   @Delete(':id')
   async delete(@Param() params) {
-    return {
-      method: 'delete',
-      params,
-    };
+    return { params };
   }
 }
