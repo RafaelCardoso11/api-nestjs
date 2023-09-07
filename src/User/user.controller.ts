7fc5647709058919fe9734a85a97a4e03d8ab589
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-07 12:08:22 -0300

    feat: crud user

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,9 +1,54 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+  @Get()
+  async list() {
+    return {
+      users: [],
+    };
+  }
+  @Get(':id')
+  async show(@Param() params) {
+    return {
+      ...params,
+      user: {},
+    };
+  }
+  @Put(':id')
+  async updateAll(@Param() params, @Body() body) {
+    return {
+      method: 'put',
+      params,
+      body,
+    };
+  }
+  @Patch(':id')
+  async update(@Param() params, @Body() body) {
+    return {
+      method: 'patch',
+      params,
+      body,
+    };
+  }
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+      method: 'delete',
+      params,
+    };
   }
 }
