f065927644a4e5b44bc163302f1aec2877e7bb41
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-18 14:29:57 -0300

    feat: interceptor log

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -3,10 +3,19 @@ import { Observable, tap } from 'rxjs';
 
 export class LogInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const dt = Date.now();
+    const dtBefore = Date.now();
+
     return next.handle().pipe(
       tap(() => {
-        console.log('execução levou: ' + (Date.now() - dt));
+        const dtAfter = Date.now();
+
+        const { method, url }: Request = context.switchToHttp().getRequest();
+
+        console.log(
+          `| ${method} - ${url} | execução na rota levou: ${
+            dtAfter - dtBefore
+          } milisegundos`,
+        );
       }),
     );
   }
