27c7a45c4984b573299214779211cf06d222b366
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-27 02:44:07 -0300

    refactor:  @Roles(Role.Admin)

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -21,37 +21,33 @@ import { Role } from 'src/enums/role.enum';
 import { RoleGuard } from 'src/guards/role.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 
+@Roles(Role.Admin)
 @UseGuards(AuthGuard, RoleGuard)
 @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Roles(Role.Admin)
   @Get()
   async list() {
     return this.userService.list();
   }
 
-  @Roles(Role.Admin)
   @Get(':id')
   async show(@Param('id', ParseIntPipe) id) {
     return this.userService.show(id);
   }
 
-  @Roles(Role.Admin)
   @Post()
   async create(@Body() body: CreateUserDTO) {
     return this.userService.create(body);
   }
 
-  @Roles(Role.Admin)
   @Put(':id')
   async update(@Param('id', ParseIntPipe) id, @Body() body: UpdatePutUserDTO) {
     return this.userService.update(id, body);
   }
 
-  @Roles(Role.Admin)
   @Patch(':id')
   async updatePartial(
     @Param('id', ParseIntPipe) id,
@@ -66,7 +62,6 @@ export class UserController {
     });
   }
 
-  @Roles(Role.Admin)
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id) {
     return this.userService.delete(id);
