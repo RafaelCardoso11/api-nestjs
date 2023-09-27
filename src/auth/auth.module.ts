e56c581fa18bb31bfcba95527ddb395023e2e7f0
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-27 03:21:01 -0300

    feat: secret from environment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   imports: [
     JwtModule.register({
-      secret: `Cx5Aw|}5G_'_j]Tf9T~6jo<$(8335W19`,
+      secret: process.env.JWT_SECRET,
     }),
     forwardRef(() => UserModule),
     PrismaModule,
